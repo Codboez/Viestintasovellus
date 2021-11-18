b7dd38b38adc758ace2e3272ec5fc60d7bf6d96a
Jerry Lehtinen <jerry.lehtinen02@gmail.com>
2021-11-18 20:25:39 +0200

    Created register user functionality

--- routes.py
+++ routes.py
@@ -1,6 +1,22 @@
 from app import app
-from flask import redirect, render_template
+from flask import redirect, render_template, request
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register/send", methods=["POST"])
+def send_register():
+    if users.register(request.form["username"], request.form["password"], request.form["confirm_password"]):
+        return redirect("/")
+    else:
+        return redirect("/register")
---
+++ users.py
@@ -0,0 +1,48 @@
+from db import db
+import re
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def register(username, password, confirmed_password) -> bool:
+    if len(username) < 1 or len(username) > 32:
+        return False
+
+    if username_exists(username):
+        return False
+
+    if not is_password_valid(password):
+        return False
+
+    if not passwords_match(password, confirmed_password):
+        return False
+
+    insert_user()
+    return True
+
+def username_exists(username: str) -> bool:
+    sql = "SELECT username FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+
+    return result.fetchone()
+
+def is_password_valid(password: str) -> bool:
+    if len(password) > 32 or len(password) < 8:
+        return False
+
+    if not re.search("\d", password):
+        return False
+
+    if not re.search("[a-z]", password):
+        return False
+
+    if not re.search("[A-Z]", password):
+        return False
+
+    return True
+
+def passwords_match(password: str, confirmed_password: str) -> bool:
+    return password == confirmed_password
+
+def insert_user(username, password):
+    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+    db.session.execute(sql, {"username":username, "password":generate_password_hash(password)})
+    db.session.commit()
