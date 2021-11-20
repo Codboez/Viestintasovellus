9ebb8bbc8c1d7caff06627e568ab72b231ed2f49
Jerry Lehtinen <jerry.lehtinen02@gmail.com>
2021-11-20 21:03:59 +0200

    Added more tables to database

--- schema.sql
+++ schema.sql
@@ -1,1 +1,26 @@
-CREATE TABLE users(id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username TEXT,
+	password TEXT
+);
+
+CREATE TABLE threads (
+	id SERIAL PRIMARY KEY,
+	creator_id INTEGER,
+	creation_time TIMESTAMP,
+	is_public BOOLEAN
+);
+
+CREATE TABLE messages (
+	id SERIAL PRIMARY KEY,
+	message TEXT,
+	sender_id INTEGER,
+	thread_id INTEGER,
+	creation_time TIMESTAMP
+);
+
+CREATE TABLE thread_users (
+	user_id INTEGER,
+	thread_id INTEGER
+);
+
